be5f24d9b7dc21d585b93b3ac7be4192558a7dca
Yeom-JinHo <pwlsghq@naver.com>
2022-01-16 10:46:04 +0900

    feat:MainPage

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,38 @@
 import "./Home.scss";
+import logoImg from "../../assets/images/volume.png";
+import { useState } from 'react';
+import SignInModal from './SignInModal';
+import SignUpModal from './SignUpModal';
+
 
 function Home() {
-  return <div className="home-container">home</div>;
+
+  const [signInModalVisible, setsingInModalVisible] = useState(false);
+  const [signUpModalVisible, setsingUpModalVisible] = useState(false);
+  const handleSignInModalVisible = () => {
+    console.log('in Togg')
+    setsingInModalVisible(!signInModalVisible)
+  }
+  const handleSignUpModalVisible = () => {
+    console.log('up Togg')
+    setsingUpModalVisible(!signUpModalVisible)
+  }
+  return (
+    < div className="homeContainer" >
+      {signInModalVisible && <SignInModal handleSignInModalVisible={handleSignInModalVisible} />}
+      {signUpModalVisible && <SignUpModal handleSignUpModalVisible={handleSignUpModalVisible} />}
+      <header className="headerContainer">
+        <div className="logoTitle">Volume</div>
+        <div className="signUpBtn" onClick={handleSignUpModalVisible}>Sign up</div>
+        <div className="signInBtn" onClick={handleSignInModalVisible}>Sign in</div>
+      </header>
+      <main className="mainContainer">
+        <div className="logoTitle">Volume</div>
+        <img className="logoImg" src={logoImg} alt='로고이미지' />
+        <div className="signUpBtn" onClick={handleSignUpModalVisible}><p>Sign Up</p></div>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
