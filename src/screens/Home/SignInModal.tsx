ff0cef57f6536dcfe6af18a10069701eeb6271d7
Yeom-JinHo <pwlsghq@naver.com>
2022-01-19 22:39:43 +0900

    feat: Modal

--- src/screens/Home/SignInModal.tsx
+++ src/screens/Home/SignInModal.tsx
@@ -3,40 +3,41 @@ import "./SignInModal.scss";
 
 const SignInModal = (props: any) => {
   return (
-    <div className="SignIn" onClick={props.handleSignInModalVisible}>
-      <div className="modalContainer" onClick={e => e.stopPropagation()}>
-        <h1 className="loginTitle">Login</h1>
-        <div className="loginContainer">
+    <div className="signin-dimmer" onClick={props.handleSignInModalVisible}>
+      <div className="modal-container" onClick={e => e.stopPropagation()}>
+        <h1 className="modal-container__login-title ">Login</h1>
+        <div className="login-form">
           <input
             type="text"
-            className="emailInput loginInput"
+            className="login-form__email-input"
             placeholder=" 이메일을 입력해주세요."
           />
           <input
             type="password"
-            className="passwordInput loginInput"
+            className="login-form__password-input"
             placeholder=" 비밀번호를 입력해주세요."
           />
-          <div className="continueBtn btn">
-            <p>계속</p>
-          </div>
-          <div className="signUpContainer">
-            <div className="findAccountBtn btn">이메일,비밀번호찾기</div>
-            <div className="signUpBtn btn">회원가입하기</div>
-          </div>
+          <div className="login-form__message">아이디 또는 비밀번호가 달라요.</div>
+          <button className="login-form__continue-btn modal-container__continue-btn">
+            계속
+          </button>
         </div>
-        <div className="or">또는</div>
-        <div className="simpleLoginContainer">
-          <p>간편로그인</p>
-          <div className="googleLoginBtn btn">
-            <p>구글로 시작하기</p>
-          </div>
-          <div className="kakaoLoginBtn btn">
-            <p>카카오톡으로 시작하기</p>
-          </div>
+        <div className="signup-wrapper">
+          <button className="signup-wrapper__find-account-btn">이메일,비밀번호찾기</button>
+          <button className="signup-wrapper__signup-btn ">회원가입하기</button>
+        </div>
+        <p className="modal-container__or">또는</p>
+        <div className="simple-login-form">
+          간편로그인
+          <button className="simple-login-form__google-login-btn ">
+            구글로 시작하기
+          </button>
+          <button className="simple-login-form__kakao-login-btn ">
+            카카오톡으로 시작하기
+          </button>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
--- src/screens/Home/SignUpModal.tsx
+++ src/screens/Home/SignUpModal.tsx
@@ -2,35 +2,39 @@ import React from "react";
 import "./SignUpModal.scss";
 const SignUpModal = (props: any) => {
   return (
-    <div className="SignUp" onClick={props.handleSignUpModalVisible}>
-      <div className="modalContainer" onClick={e => e.stopPropagation()}>
-        <h1 className="signUpTitle">Sign up</h1>
+    <div className="signup-dimmer" onClick={props.handleSignUpModalVisible}>
+      <div className="modal-container" onClick={e => e.stopPropagation()}>
+        <h1 className="modal-container__signup-title">Sign up</h1>
+        <p className="modal-container__sub-title">간단한 회원가입을 통해 서비스를 즐겨보세요.</p>
         <input
           type="text"
-          className="emailInput"
-          placeholder="이메일을 입력해주세요."
+          className="modal-container__id-input"
+          placeholder="아이디"
         />
-        <input
-          type="text"
-          className="emailAuthInput"
-          placeholder="이메일 인증코드를 입력해주세요"
-        />
-
-        <hr />
         <input
           type="password"
-          className="passwordInput"
+          className="modal-container__password-input"
           placeholder="비밀번호를 입력해주세요"
         />
         <input
           type="password"
-          className="passwordAuthInput"
+          className="modal-container__password-auth-input"
           placeholder="다시 비밀번호를 입력해주세요"
         />
-        <hr />
-        <div className="continueBtn btn">
-          <p>계속</p>
-        </div>
+        <p className="modal-container__password-comment">영 대소문자,숫자를 홉합해 8-15로 입력해주세요.</p>
+        <input
+          type="text"
+          className="modal-container__name-input"
+          placeholder="이름"
+        />
+        <input
+          type="text"
+          className="modal-container__email-input"
+          placeholder="이메일"
+        />
+        <button className="modal-container__continue-btn">
+          계속
+        </button>
       </div>
     </div>
   );
