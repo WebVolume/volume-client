0f91651803579a3d5f5ae36390f1c1b06583884c
Yeom-JinHo <pwlsghq@naver.com>
2022-01-29 21:21:10 +0900

    소셜로그인 모달 리네임

--- src/components/SignModal/SocialSignUpModal/SocialSignUpModal.tsx
+++ src/components/SignModal/SocialSignUpModal/SocialSignUpModal.tsx
@@ -3,27 +3,30 @@ import "./SocialSignUpModal.scss";
 const SocialSignUpModal = (props: any) => {
   return (
     <div className="signup-dimmer" onClick={props.handleSignUpModalVisible}>
-      <div className="modal-container" onClick={e => e.stopPropagation()}>
-        <h1 className="modal-container__signup-title">Sign up</h1>
-        <p className="modal-container__sub-title">
+      <div
+        className="social-modal-container"
+        onClick={e => e.stopPropagation()}
+      >
+        <h1 className="social-modal-container__signup-title">Sign up</h1>
+        <p className="social-modal-container__sub-title">
           간단한 회원가입을 통해 서비스를 즐겨보세요.
         </p>
         <input
           type="text"
-          className="modal-container__id-input"
+          className="social-modal-container__id-input"
           placeholder="아이디"
         />
         <input
           type="text"
-          className="modal-container__name-input"
+          className="social-modal-container__name-input"
           placeholder="이름"
         />
         <input
           type="text"
-          className="modal-container__email-input"
+          className="social-modal-container__email-input"
           placeholder="이메일"
         />
-        <button className="modal-container__continue-btn">계속</button>
+        <button className="social-modal-container__continue-btn">계속</button>
       </div>
     </div>
   );
