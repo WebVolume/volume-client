71fef208098683e52262c07c086a759858521572
Yeom-JinHo <pwlsghq@naver.com>
2022-02-12 20:04:21 +0900

    이 곡이 있는 플레이 리스트

--- src/components/Common/InPlayLIst/InPlayList.tsx
+++ src/components/Common/InPlayLIst/InPlayList.tsx
@@ -3,14 +3,22 @@ import "./InPlayList.scss";
 import MiniPlayList from "@components/Common/MiniPlayList";
 const InPlayList = () => {
   return (
-    <div className="in-play-list">
-      <div className="in-play-list__header">
-        <div className="in-play-list__title">이 곡이 있는 플레이 리스트</div>
-        <button className="in-play-list__detail-btn">더보기</button>
+    <div className="in-play-list mont">
+      <div className="in-play-list__header flex align-center">
+        <p className="in-play-list__title fs-18">이 곡이 있는 플레이 리스트</p>
+        <a href="" className="in-play-list__detail-btn fs-14">
+          더보기
+        </a>
       </div>
-      <MiniPlayList />
-      <MiniPlayList />
-      <MiniPlayList />
+      <ul className="mini-play-list-ul">
+        {[1, 2, 3].map(list => {
+          return (
+            <li>
+              <MiniPlayList />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
