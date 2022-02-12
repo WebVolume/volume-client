261979e97910e841728264a96fa86271d719de62
minsoo kim <zlemzlem5656@naver.com>
2022-02-12 16:29:36 +0900

    feat: 메인 레이아웃 수정 -- 상단바

--- src/screens/Main/Dashboard/Dashboard.tsx
+++ src/screens/Main/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import "./Dashboard.scss";
 import { MainSwiper } from "@components/Dashboard";
+import * as Common from "@components/Common";
 
 function Dashboard() {
   return (
     <div className="dashboard-screen">
       <MainSwiper />
+      {/* <Common.RightAside /> */}
     </div>
   );
 }
