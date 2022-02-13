af048600d8f59fc8fa7c0690a997bbebeaeb17c6
minsoo kim <zlemzlem5656@naver.com>
2022-02-13 23:20:17 +0900

    feat: 클릭 가능한 요소 처리

--- src/components/Dashboard/PopularVolumerList/PopularVolumerListItem/VolumerPlaylist/VolumerPlaylist.tsx
+++ src/components/Dashboard/PopularVolumerList/PopularVolumerListItem/VolumerPlaylist/VolumerPlaylist.tsx
@@ -12,7 +12,9 @@ function VolumerPlaylist() {
           <div className="volumer-play-list-item--container flex align-center">
             <img className="album-art" src={DummyImage} alt="앨범 아트" />
             <span className="f-one fs-12 mont-alt">Dummy Music Title</span>
-            <img className="play-btn" src={PlayGreyImg} alt="재생 버튼" />
+            <a href="#" title="재생">
+              <img className="play-btn" src={PlayGreyImg} alt="재생 버튼" />
+            </a>
           </div>
         </li>
       ))}
